e83286784e2eda2071d07a2be04bf2d85a08ddd3
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-07-29 16:08:52 +0300

    Added changes

--- ganzapay-dashboard/src/App.js
+++ ganzapay-dashboard/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; 
 import CustomerCare from './Customercare';
 import './App.css';
 
-
 function App() {
- return (
-   <div className="App">
-     <CustomerCare/>
-   </div>
- );
+  return (
+    <BrowserRouter> 
+      <div className="App">
+        <CustomerCare />
+      </div>
+    </BrowserRouter>
+  );
 }
-
 
 export default App;
