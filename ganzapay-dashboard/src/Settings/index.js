092ee58e246eede8ea9b3bee079b8f729a802d5a
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-07-31 11:53:36 +0300

    added toggle switch

--- ganzapay-dashboard/src/Settings/index.js
+++ ganzapay-dashboard/src/Settings/index.js
@@ -1,14 +1,9 @@
+
 import './index.css';
 import React, { useState } from 'react';
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { GrLanguage } from "react-icons/gr";
 import { IoIosNotifications } from "react-icons/io";
-import { IoHomeOutline } from 'react-icons/io5';
-import { FcStatistics } from "react-icons/fc";
-import { IoIosLogOut } from "react-icons/io";
-import { CiDollar } from "react-icons/ci";
-import { RiCustomerService2Line } from "react-icons/ri";
-import { IoSettingsOutline } from "react-icons/io5";
 import { HiChevronDown } from "react-icons/hi";
 const Settings = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -19,37 +14,10 @@ const Settings = () => {
   const handleLanguageChange = (e) => {
     setSelectedLanguage(e.target.value);
   };
-
   return (
     <div className={darkMode ? 'dark-mode' : 'light-mode'}>
       <h1>Settings</h1>
       <div className='container'>
-        <div className="sections">
-          <div className="home">
-            <IoHomeOutline />
-            <p>Home</p>
-          </div>
-          <div className="transaction">
-            <CiDollar />
-            <p>Transaction</p>
-          </div>
-          <div className="statistics">
-            <FcStatistics />
-            <p>Statistics</p>
-          </div>
-          <div className="care">
-            <RiCustomerService2Line />
-            <p>Customer Care</p>
-          </div>
-          <div className="setting">
-            <IoSettingsOutline />
-            <p>Settings</p>
-          </div>
-          <div className="log">
-            <IoIosLogOut />
-            <p>Log Out</p>
-          </div>
-        </div>
         <div className='heading'>
           <h2>Accounts</h2>
           <div className='customer'>
@@ -57,40 +25,52 @@ const Settings = () => {
               <IoPersonCircleOutline />
               <p> Customer Name</p>
             </div>
-            <HiChevronDown />
-          </div>
-          <div className='language'>
-            <div className='icon-language'>
-              <GrLanguage />
-              <p>Language</p>
-            </div>
+
+   <div className='drop1'>
+      <HiChevronDown />
+        </div>
+
+    </div>
+      <div className='language'>
+        <div className='icon-language'>
+          <GrLanguage />
+          <p>Language</p>
+        </div>
+        
+        <div className='select-container'>
             <select value={selectedLanguage} onChange={handleLanguageChange}>
               <option value="English">English</option>
               <option value="Kinyarwanda">Kinyarwanda</option>
             </select>
+            </div>
+      
           </div>
           <div className='notify'>
             <div className='icon-notify'>
               <IoIosNotifications />
               <p>Notifications</p>
             </div>
-            <HiChevronDown />
-            <br></br>
-          </div>
-          <div className='mode'>
-            <IoIosNotifications />
-            <p>Dark Mode</p>
-            <label className="switch">
-              <input type="checkbox" onChange={toggleDarkMode} checked={darkMode} />
-              <span className="slider round"></span>
-            </label>
 
+            <div className='drop2'>
+            <HiChevronDown />
+            </div>
+            <br/>
+          </div>
+
+<div className='mode'>
+        <div className='toggle-container'>
+          <p>Dark Mode</p>
+          <div className={`toggle-switch ${darkMode ? 'active' : ''}`} onClick={toggleDarkMode}>
+            <div className='slider'></div>
           </div>
         </div>
       </div>
-    </div>
 
-    
+          
+        </div>
+      </div>
+    </div>
   );
 }
 export default Settings;
+
